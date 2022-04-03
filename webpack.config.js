c5c30f5e89b1746d3cde5ec6e7b535f5b5d833a7
margherita95444 <mariamargheritapanzani@gmail.com>
2022-04-03 11:55:23 +0200

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js', 
   mode: "development",
+  module: {     
+    rules: [    
+      {        
+        test: /\.m?js$/,  
+        exclude: /node_modules/,
+        use: [ 
+          {loader: "babel-loader"}
+        ]
+      }
+    ]
+  },
   output: {             
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   }
 };
+
+
