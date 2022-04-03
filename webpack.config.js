39a878f6c5706c2337a1df022f8c1bad8055c290
margherita95444 <mariamargheritapanzani@gmail.com>
2022-04-03 12:10:43 +0200

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
       {        
         test: /\.m?js$/,  
         exclude: /node_modules/,
-        use: [ 
-          {loader: "babel-loader"}
-        ]
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   },
